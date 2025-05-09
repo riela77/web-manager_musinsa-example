f3f12ee31a7dc4bbf0f4b3069db4a84ac7d7ad51
riela77 <143330718+riela77@users.noreply.github.com>
2025-05-09 18:02:08 +0900

    Add files via upload

---
+++ backend/product/productDAO.py
@@ -0,0 +1,62 @@
+from DBconnectLibrary.kimDBmanager import kimDBmanager
+class productDAO:
+    def get(self):
+        con, cur = None, None  # 초기화
+        try:
+            con, cur = kimDBmanager.makeConCur("KIMCR/1@195.168.9.126:1521/xe")
+            sql = "SELECT * FROM product_M ORDER BY p_name DESC"
+
+            cur.execute(sql)
+            products = []
+            for p_id, p_name, p_brand_id, p_category_id, p_price, p_image, p_location in cur:
+                p = {
+                    "p_id": p_id,
+                    "p_brand_id": p_brand_id,
+                    "p_category_id": p_category_id,
+                    "p_price": p_price,
+                    "p_image": p_image,
+                    "p_location": p_location,
+                    "p_name": p_name
+                }
+                products.append(p)
+            return products
+        except Exception as e:
+            print("조회 실패:", e)
+            return {"result": "조회 실패"}
+        finally:
+            if con and cur:
+                kimDBmanager.closeConCur(con, cur)
+
+    def product_reg(p_id, p_name, brand_id, category_id, price, image, location):
+        con, cur = None, None
+        try:
+            con, cur = kimDBmanager.makeConCur("KIMCR/1@195.168.9.126:1521/xe")
+            cur.execute("""
+                INSERT INTO product_M 
+                (p_id, p_name, brand_id, category_id, price, image_url, store_location)
+                VALUES (:1, :2, :3, :4, :5, :6, :7)
+            """, (p_id, p_name, brand_id, category_id, price, image, location))
+            con.commit()
+            return {"result": "상품 등록 완료"}
+        except Exception as e:
+            print("조회 실패:", e)
+            return {"result": "조회 실패"}
+        finally:
+            if con and cur:
+                kimDBmanager.closeConCur(con, cur)
+
+
+
+    def test_connection(self):
+        con, cur = None, None
+        try:
+            con, cur = kimDBmanager.makeConCur("KIMCR/1@195.168.9.126:1521/xe")
+            print("✅ 오라클 DB 연결 성공!")
+            return {"result": "연결 성공"}
+        except Exception as e:
+            print("❌ DB 연결 실패:", e)
+            return {"result": f"연결 실패: {e}"}
+        finally:
+            if con and cur:
+                kimDBmanager.closeConCur(con, cur)
+
